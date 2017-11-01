6b74145ab749ff471b6474a8c1e30b5f6b0729a7
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-01 18:17:53 -0400

    exercise 2

---
+++ exercise22.rb
@@ -0,0 +1,16 @@
+def negative(num)
+  if
+    (num<0)
+  return true
+
+  else
+
+  return false
+
+  end
+end
+
+puts("is 2 a negative number? #{negative(2)}")
+puts("is 4 a negative number? #{negative(4)}")
+puts("is -6 negative number? #{negative(-6)}")
+puts("is -8 negative number? #{negative(-8)}")
