9022cbb87ef318582bde8a7da790672deb1ef777
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-01 18:31:05 -0400

    exercise 3

---
+++ exercise23.rb
@@ -0,0 +1,14 @@
+def is_even(num)
+  if
+    num % 2 == 0
+  return true
+
+  else
+    return false
+  end
+end
+
+puts("is 2 an even number? #{is_even(2)}")
+puts("is 3 an even number? #{is_even(3)}")
+puts("is 4 an even number? #{is_even(4)}")
+puts("is 7 an even number? #{is_even(7)}")
